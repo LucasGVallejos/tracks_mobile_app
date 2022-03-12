36e4f3324d7288e249c7859a02bd0802ac96227f
Lucas Vallejos <wlucasgv@live.com.ar>
2022-03-12 16:59:58 -0300

    Extracting Auth Logic into Form

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,42 +1,28 @@
-import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import { Context as AuthContext } from '../context/AuthContext';
+import React, {useContext} from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Text} from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
 
-const SignupScreen = ({ navigation }) => {
-    const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const SignupScreen = ({navigation}) => {
+    const {state, signup} = useContext(AuthContext);
 
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
-            <Spacer />
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            {state.errorMessage ? (
-                <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-            ) : null}
-
-            <Spacer>
-                <Button title="Sign Up" onPress={() => signup({ email, password })} />
-            </Spacer>
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>
+                        Already have an account? Sign in instead
+                    </Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -53,13 +39,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginBottom: 10,
     },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15,
+    link: {
+        color: 'blue',
     },
-
 });
 
 export default SignupScreen;
